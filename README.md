695f50ee64f277ff22f5e0f052ccaebacc246cf5
pritikavig <pritikavig@aol.com>
2016-03-05 15:23:51 -0500

    adding powerpoint link

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # KaggleBNP
 Kaggle BNP Paribas Competition
 
-# Algorithms
-Pritika -> decision tree
+Google Doc Presentation Link:
+https://docs.google.com/presentation/d/1ftxbRtFZ4w5nEv_afZ1bAtCeQ0TBn7Fvo6zqN4n9sD8/edit?usp=sharing
+
+
+algorithms implemented:
+pv:
+- naive bayes
+- svm
--- pritika/mix.py
+++ pritika/mix.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np 
 from naive_bayes import naive_bayes
 from svm import bnp_svm
+import operator
 
 
 
@@ -13,19 +14,13 @@ yhat1 = naive_bayes(train, test)
 print("svm")
 yhat2 = bnp_svm(train, test)
 
-print(yhat1)
-print(yhat2)
 
 
-
-weight = [2, 2]
-yhat1 = np.divide(yhat1, weight[0])
-yhat2 = np.divide(yhat2, weight[1])
-
-	
-pred = np.add(yhat1, yhat2)
-print(pred)
-
+def applyWeights(yhat1, yhat2, weight):
+	yhat1 = np.divide(yhat1, weight[0])
+	yhat2 = np.divide(yhat2, weight[1])
+	pred = np.add(yhat1, yhat2)
+	return pred
 
 
 def getLabel(ar):
@@ -36,7 +31,41 @@ def getLabel(ar):
 		label.append(round(x[1]))
 	return label,softlabel
 
-lab,soft = getLabel(pred)
-print(sum(lab))
-print(len(lab))
+def getError(pred, true):
+	print(pred)
+	print(true)
+	total = len(pred)
+	er = 0.0
+	for i in range(len(pred)):
+		if pred[i] != true[i][1]:
+			er += 1
+	return er/float(total)
 
+def cross_val(n, train, weight):
+	print("cv")
+	err = []
+	for i in range(n):
+		train_sub = train.sample(10000)
+		test_sub = train.sample(1000)
+		test_y = test_sub['target']
+
+		y1 = bnp_svm(train_sub, test_sub)
+		y2 = naive_bayes(train_sub, test_sub)
+		pred = applyWeights(y1, y2, weight)
+		lab,s = getLabel(pred)
+		err.append(getError(lab, test_y))
+	return (sum(err)/float(len(err)))
+
+
+def selectWeights(train):
+	print("sw")
+	weights = [[0.1,0.9], [0.2,0.8], [0.3,0.7], [0.4,0.6], [0.5,0.5], [0.6,0.4], [0.7, 0.3], [0.8,0.2], [0.9,0.1]]
+	error = []
+	for weight in weights:
+
+		error.append(cross_val(3, train, weight))
+	min_index, min_value = min(enumerate(error), key=operator.itemgetter(1))
+	return(weights[min_index])
+
+print("cross val to select weights")
+print(selectWeights(train))
